da3e67b45254506ad7f797147d6e62125a9f8f87
nevaehph <tph@t05.sg>
2020-08-21 11:47:57 +0800

    Remove subview = null

--- packages/react-native-external-display/android/src/main/java/com/externaldisplay/RNExternalDisplayView.java
+++ packages/react-native-external-display/android/src/main/java/com/externaldisplay/RNExternalDisplayView.java
@@ -74,13 +74,14 @@ public class RNExternalDisplayView extends ReactViewGroup implements LifecycleEv
       if (wrap != null && wrap.getChildCount() > 0) {
         wrap.removeViewAt(0);
       }
-      subview = null;
+      //subview = null;
     }
     destroyScreen();
   }
 
   @Override
   public void onHostResume() {
+    updateScreen();
   }
 
   @Override
